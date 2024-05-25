4e5a83dee24b4cdd08dfd744d03d454674cae5d0
HwangSeungMin <30570119+HwangSeungMin@users.noreply.github.com>
2024-05-25 23:12:46 +0900

    StudentManagementSystem
    Change Of System Method : searchStudent
    
    this , private , getter , setter , Method , Constructor , Object , Instance

--- Week2-Day2/src/week2day2/StudentManagementSystem.java
+++ Week2-Day2/src/week2day2/StudentManagementSystem.java
@@ -7,18 +7,18 @@ import java.util.Map;
 import week2day2.Student;
 public class StudentManagementSystem {
     private final int maxCntStudent;
-    private final Map<Integer,Student> students = new HashMap<Integer,Student>();
+    private final Map<String,Student> students = new HashMap<String,Student>();
 
     StudentManagementSystem(int maxCntStudent) {
         this.maxCntStudent = maxCntStudent;
     }
 
     public void addStudent(Student student) {
-        if(this.students.containsKey(student.getId())){
+        if(this.students.containsKey(String.valueOf(student.getId()))){
             System.out.println("이미 등록된 학번의 학생 입니다."+student.getId());
         }else {
             if (students.size() < this.getMaxCntStudent()) {
-                this.students.put(student.getId(),student);
+                this.students.put(String.valueOf(student.getId()),student);
                 System.out.println("학생 추가 :" + student.getName());
             } else {
                 System.out.println("최대 정원 " + this.getMaxCntStudent() + " 명의 학생이 등록되어 있습니다.");
@@ -27,8 +27,8 @@ public class StudentManagementSystem {
     }
 
     public void removeStudent(Student student) {
-        if(this.students.containsKey(student.getId())){
-            this.students.remove(Integer.valueOf(student.getId()));
+        if(this.students.containsKey(String.valueOf(student.getId()))){
+            this.students.remove(String.valueOf(student.getId()));
             System.out.println("학생 삭제 : "+student.getName());
         }else {
             System.out.println("학생을 찾을 수 없습니다. 학번 : "+student.getId());
@@ -36,8 +36,8 @@ public class StudentManagementSystem {
     }
 
     public void searchStudent(int studentId) {
-        if(this.students.containsKey(studentId)){
-            Student st1 = this.students.get(studentId);
+        if(this.students.containsKey(String.valueOf(studentId))){
+            Student st1 = this.students.get(String.valueOf(studentId));
             System.out.println("이름 : "+st1.getName());
             System.out.println("학번 : "+st1.getId());
             System.out.println("전공 : "+st1.getMajor());
